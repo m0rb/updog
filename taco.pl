8979d51754b0ba4467b5b3fc54245cb9d4140c26
m0rb <chris@cataclysmal.org>
2016-03-26 11:45:37 -0700

    Create taco.pl
    tacos.

---
+++ taco.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+# taco.pl - DM yourself, receive a shortened t.co URL
+# usage: ./taco.pl http://example.com
+#
+# 3/26/16 - chris_commat_misentropic_commercial
+use warnings;
+use Net::Twitter;
+
+my $nt = Net::Twitter->new(
+traits   => [qw/API::RESTv1_1/],
+          consumer_key        => '',
+          consumer_secret     => '',
+          access_token        => '',
+          access_token_secret => '',
+          ssl => '1',
+);
+
+die unless $nt->authorized;
+# note: you'll need to swap out the example user_id with your own.
+my $t = $nt->new_direct_message({ user_id => 12345678, text => @ARGV });
+print "$t->{text}\n";
